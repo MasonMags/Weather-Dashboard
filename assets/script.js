d2780f7c5d25bea98cf173d9df21468637c8298c
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-05-18 16:32:02 -0500

    finished fetch function, displayed data on page with dynamic html

--- assets/script.js
+++ assets/script.js
@@ -10,4 +10,39 @@ function fetchWeatherData() {
     var weatherQueryString = weatherURL.concat(locationInput) + "&Appid=" + apiKey + "&units=imperial"
 
     fetch(weatherQueryString)
+    .then(res => res.json())
+    .then(function (data){
+        var forecast = data.main
+        var icon = data.weather[0].icon
+        var weatherDate = document.createElement('p')
+        var weatherCurrentTemp = document.createElement('p')
+        var weatherHumidity = document.createElement('p')
+        var weatherMaxTemp = document.createElement('p')
+        var weatherMinTemp = document.createElement('p')
+        var weatherIcon = document.createElement('img')
+        var iconURL = "https://openweathermap.org/img/wn/"
+        
+       // weatherDate.textContent = moment().format("dddd, MMMM Do")
+        //weatherDate.classList.add("weather-date")
+    
+        weatherCurrentTemp.textContent = "Current Temp: " + forecast.temp
+        weatherCurrentTemp.classList.add("weather-current-temp");
+        
+        weatherHumidity.textContent = "Humidity: " + forecast.humidity
+        weatherHumidity.classList.add("weather-humidity");
+    
+        weatherMaxTemp.textContent =  "High: " + forecast.temp_max + "° F"
+        weatherMaxTemp.classList.add("weather-max-temp");
+    
+        weatherMinTemp.textContent = "Low: " + forecast.temp_min + "° F"
+        weatherMinTemp.classList.add("weather-min-temp");
+    
+        
+        iconimport = iconURL.concat(icon)
+        weatherIcon.setAttribute("src" , iconimport+".png")
+        weatherIcon.classList.add("weather-icon");
+    
+        weatherContainer.append(weatherDate, weatherCurrentTemp, weatherHumidity, weatherMaxTemp, weatherMinTemp)
+        weatherContainer.append(weatherIcon)
+    })
 }
