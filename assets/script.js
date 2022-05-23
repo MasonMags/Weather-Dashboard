8c36541c47d96fcd1a55f0ba9bedc11fe23d010a
Mason Maglalang <MasonMaglalang1@gmail.com>
2022-05-23 16:15:08 -0500

    finished fetch forecast function

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,10 @@ var searchContainer = document.getElementById("search-container")
 var forecastContainer = document.getElementById("forecast-container")
 
 var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q="
+var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="
 var apiKey = "126e4065d97fedad97742cdb5c363ca9"
 
-function fetchWeatherData() {
+function fetchCurrentData() {
     var locationInput = document.getElementById("location-picker").value
     var weatherQueryString = weatherURL.concat(locationInput) + "&Appid=" + apiKey + "&units=imperial"
 
@@ -43,6 +44,37 @@ function fetchWeatherData() {
         weatherIcon.classList.add("weather-icon");
     
         weatherContainer.append(weatherDate, weatherCurrentTemp, weatherHumidity, weatherMaxTemp, weatherMinTemp)
-        weatherContainer.append(weatherIcon)
+        weatherContainer.append(weatherIcon);
+
+        fetchForecastData();
+    })
+}
+
+function fetchForecastData() {
+    var locationInput = document.getElementById("location-picker").value
+    var weatherQueryString = forecastURL.concat(locationInput) + "&Appid=" + apiKey + "&units=imperial"
+
+    fetch(weatherQueryString)
+    .then(res => res.json())
+    .then(function (data){
+        console.log(data.list)
+        var forecast = data.list
+    for (var i = 3; i < forecast.length; i+=8) {
+        var date = document.createElement('p')
+        date.textContent = forecast[i].dt_txt
+        forecastContainer.append(date);
+
+        var temp = document.createElement('p')
+        temp.textContent = "Temperature: " + forecast[i].main.temp + "° F"
+        forecastContainer.append(temp);
+
+        var wind = document.createElement('p')
+        wind.textContent = "Wind: " + forecast[i].wind.speed + " MPH"
+        forecastContainer.append(wind)
+
+        var humidity = document.createElement('p')
+        humidity.textContent = "Humidity: " + forecast[i].main.humidity + "%"
+        forecastContainer.append(humidity)
+    }
     })
 }
